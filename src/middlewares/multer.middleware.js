f8024e0a6517d958635aaee0af653a5c180178f2
SagarBohat <sagarkumar76451@gmail.com>
2025-01-17 01:16:55 +0530

    File upload setup, cloudinary and multer setup.

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+
+const diskStorage  = multer.diskStorage({
+    destination:function (req,file,callback){
+        callback(null,"./public/temp")
+    },
+    filename:function (req,file,callback) {
+     const uniqueSuffix = Date.now()+"-"+Math.round(Math.random()*1E9)
+     const  fileName = file.filename+"-"+uniqueSuffix
+     callback(null,fileName)
+    }
+})
+
+export const upload  = multer({storage:diskStorage})
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,35 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs  from 'fs';
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:CLOUDINARY_API_KEY,
+    api_secret:CLOUDINARY_API_SECRET
+})
+
+ async function uploadFile(fileLocation=''){
+    try {
+        if(fileLocation) {
+            const response =  await cloudinary.uploader.upload(fileLocation,{resource_type:"auto"})
+
+            if(response) {
+                console.log("File uploaded successfully,: ",response.url)
+                return response
+            }else {
+                console.log("File not uploaded succesfuly!!!")
+                return null
+            }
+
+        }else {
+            console.log("Local file locaiton not found !!!")
+            return null
+        }
+    } catch (error) {
+        fs.unlinkSync(fileLocation)
+        console.log("Error in cloudinary file upload : ",error)
+        return null
+    }
+
+}
+
+export {uploadFile}
